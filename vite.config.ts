a88787b9a5e5ff6ce30cd5768cbb48c70437ec66
rohilpatel15 <rohilpatel123@outlook.com>
2025-02-19 19:04:18 -0800

    Changed base to repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bollywoodbounty/',
+  base: 'https://rohilpatel15.github.io/bollywoodbounty/',
 })
